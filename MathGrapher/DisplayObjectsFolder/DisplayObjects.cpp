ef9fe89742e21b9385de3f3e1da49e65942eed7c
AlliterativeAnchovies <alliterative.anchovies@gmail.com>
2018-01-04 11:19:32 -0800

    pushing to undo

--- MathGrapher/DisplayObjectsFolder/DisplayObjects.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  DisplayObjects.cpp
-//  MathGrapher
-//
-//  Created by Bailey Andrew on 29/12/2017.
-//  Copyright © 2017 Alliterative Anchovies. All rights reserved.
-//
-
-#include "DisplayObjects.hpp"
